0608227a059a935c0398c6eb727fb081b1b46739
Mustafa Cem ALDAĞ <35099987+gyroscopium@users.noreply.github.com>
2024-02-17 00:25:36 +0300

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -2,6 +2,5 @@
 height = float(input("Enter your height in meters e.g:1.87 "))
 # 2nd input: enter weight in kilograms e.g: 72
 weight = int(input("Enter your weight in kilograms e.g: 90 "))
-BMI = weight/(height**2)
-result = round(BMI)
-print("Your BMI is: " + str(result))
+BMI = round(weight/(height**2), 1)
+print(f"Your BMI is: {BMI}")
